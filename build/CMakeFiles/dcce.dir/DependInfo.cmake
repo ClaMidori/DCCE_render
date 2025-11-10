743edf301b8448776b0bc109516a358fd4f601cb
MTomain <mtomain3@gmail.com>
2025-11-10 14:35:45 -0300

    trying to fiz things

--- build/CMakeFiles/dcce.dir/DependInfo.cmake
+++ build/CMakeFiles/dcce.dir/DependInfo.cmake
@@ -1,30 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "C"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_C
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/block.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/block.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/building.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/building.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/controls.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/controls.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/door.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/door.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/ground.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/ground.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/keyboard.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/keyboard.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/main.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/main.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/mouse.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/mouse.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/roof.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/roof.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/structures.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/structures.c.o"
-  "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/src/wall.c" "/home/mirian/UNESP/DCCE_CG/Updates/DCCE_render/build/CMakeFiles/dcce.dir/src/wall.c.o"
-  )
-set(CMAKE_C_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_C_TARGET_INCLUDE_PATH
-  "../include"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/mariatomain/CG_prj2/src/block.c" "CMakeFiles/dcce.dir/src/block.c.o" "gcc" "CMakeFiles/dcce.dir/src/block.c.o.d"
+  "/home/mariatomain/CG_prj2/src/building.c" "CMakeFiles/dcce.dir/src/building.c.o" "gcc" "CMakeFiles/dcce.dir/src/building.c.o.d"
+  "/home/mariatomain/CG_prj2/src/controls.c" "CMakeFiles/dcce.dir/src/controls.c.o" "gcc" "CMakeFiles/dcce.dir/src/controls.c.o.d"
+  "/home/mariatomain/CG_prj2/src/door.c" "CMakeFiles/dcce.dir/src/door.c.o" "gcc" "CMakeFiles/dcce.dir/src/door.c.o.d"
+  "/home/mariatomain/CG_prj2/src/ground.c" "CMakeFiles/dcce.dir/src/ground.c.o" "gcc" "CMakeFiles/dcce.dir/src/ground.c.o.d"
+  "/home/mariatomain/CG_prj2/src/keyboard.c" "CMakeFiles/dcce.dir/src/keyboard.c.o" "gcc" "CMakeFiles/dcce.dir/src/keyboard.c.o.d"
+  "/home/mariatomain/CG_prj2/src/main.c" "CMakeFiles/dcce.dir/src/main.c.o" "gcc" "CMakeFiles/dcce.dir/src/main.c.o.d"
+  "/home/mariatomain/CG_prj2/src/mouse.c" "CMakeFiles/dcce.dir/src/mouse.c.o" "gcc" "CMakeFiles/dcce.dir/src/mouse.c.o.d"
+  "/home/mariatomain/CG_prj2/src/roof.c" "CMakeFiles/dcce.dir/src/roof.c.o" "gcc" "CMakeFiles/dcce.dir/src/roof.c.o.d"
+  "/home/mariatomain/CG_prj2/src/structures.c" "CMakeFiles/dcce.dir/src/structures.c.o" "gcc" "CMakeFiles/dcce.dir/src/structures.c.o.d"
+  "/home/mariatomain/CG_prj2/src/wall.c" "CMakeFiles/dcce.dir/src/wall.c.o" "gcc" "CMakeFiles/dcce.dir/src/wall.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
