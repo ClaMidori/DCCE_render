1fd80f8b21672a2ee84c380ad87c23d61037f4f3
Mirian Jaeger <mirian@localhost>
2025-11-26 15:29:35 -0300

    Janela mais ultimas alterações

---
+++ build/CMakeFiles/dcce.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/ar_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/ar_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/cadeiraLow_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/cadeiraLow_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/cadeiraU_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/cadeiraU_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/camera_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/camera_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/computador_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/computador_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/gabinete_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/gabinete_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/mesaR_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/mesaR_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/pia_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/pia_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/privada_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/privada_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/sofa_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/sofa_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/furnitures/tcadeiras_export.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/furnitures/tcadeiras_export.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/block.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/block.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/building.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/building.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/controls.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/controls.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/door.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/door.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/furniture.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/furniture.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/ground.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/ground.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/keyboard.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/keyboard.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/main.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/main.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/mouse.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/mouse.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/roof.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/roof.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/structures.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/structures.c.o"
+  "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/src/wall.c" "/home/mirian/UNESP/CG/DCCE_3d/DCCE_render/build/CMakeFiles/dcce.dir/src/wall.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
