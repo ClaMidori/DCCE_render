601afe648edfcc692520c647af5bfcbe6b714358
AllanFull <166524645+AllanFullb@users.noreply.github.com>
2025-11-26 15:30:14 -0300

    Delete src directory

--- src/include/furniture.h
+++
@@ -1,8 +0,0 @@
-#include <GL/gl.h>
-void teste_objetos();
-void mobilia_recepcao();
-void mobilia_banheiros();
-void mobilia_salas();
-void mobilia_corredor();
-void geladeira_draw();
-void primeiro_andar_moveis();
--- src/include/roof.h
+++
@@ -1,7 +0,0 @@
-#ifndef ROOF_H
-#define ROOF_H
-
-#include "../include/commons.h"
-
-void main_roof();
-#endif
