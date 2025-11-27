f415bd87673d1263222d058d7c8ee61316df1571
AllanFull <166524645+AllanFullb@users.noreply.github.com>
2025-11-26 22:06:21 -0300

    Add files via upload

--- include/commons.h
+++ include/commons.h
@@ -25,16 +25,17 @@
 #define black glColor3f(0, 0, 0);
 #define green glColor3f(0.06f, 0.0164f, 0.102f);
 #define green_flower glColor3f(0.2, 0.7, 0.2);
+#define blue_sing glColor3f(0, 0, 0.5);
 
 //* A construção do prédio funciona como uma matriz tridimensional, pela escala, 10x10x3
 
 #define base 50 //* métrica base para a construção das salas
-#define mx 10 //* número de linhas de blocos do prédio
-#define mz 10 //* número de colunas de blocos do prédio
-#define my 2 //* altura da matriz
+#define mx 10   //* número de linhas de blocos do prédio
+#define mz 10   //* número de colunas de blocos do prédio
+#define my 2    //* altura da matriz
 
 //* constantes auxiliares
 #define wall_thickness 3
-#define p_thickness base/10
+#define p_thickness base / 10
 #define ground_offset 5
-#define betw_height 0.3*base
+#define betw_height 0.3 * base
